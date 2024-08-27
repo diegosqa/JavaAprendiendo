63636c8a83f6dc2ed3afbff89a03aa16eb09c59c
diegosqa <diego.quispe.a@uni.pe>
2024-08-27 16:18:12 -0500

    Primer push

--- PrimerProyecto/src/main/java/com/mycompany/primerproyecto/PrimerProyecto.java
+++ PrimerProyecto/src/main/java/com/mycompany/primerproyecto/PrimerProyecto.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.mavenproject1;
+package com.mycompany.primerproyecto;
 
 /**
  *
  * @author FAM-QA
  */
-public class Mavenproject1 {
+public class PrimerProyecto {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
